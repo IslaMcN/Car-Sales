9c335d80a3a37fea5810fa58373c94a117c4cc7f
IslaMcN <51002520+IslaMcN@users.noreply.github.com>
2019-09-11 16:16:49 -0700

    refactoring

--- src/actions/index.js
+++ src/actions/index.js
@@ -6,6 +6,9 @@ const toggleItem = item => {
     return{type: TOGGLE_ITEM, payload: item};
 };
 
+export const removeItem = item => {
+    return{type: REMOVE_ITEM, payload: item};
+}
 export const TOGGLE_ITEM ="TOGGLE_ITEM";
-
+export const REMOVE_ITEM ="REMOVE_ITEM";
 export default toggleItem;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {connect} from 'react-redux';
 
 const Header = props => {
   return (
@@ -12,4 +13,10 @@ const Header = props => {
   );
 };
 
-export default Header;
+const mapStateToProps =({car}) => {
+  return {image: car.image,
+     name: car.name, 
+     price: car.price}
+}
+
+export default connect(mapStateToProps, {} )(Header);
